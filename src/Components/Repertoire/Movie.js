b316dfc18dafee81e46efaeb400c9fd88541ccd9
ZygmuntJakub <216938@edu.p.lodz.pl>
2019-12-15 17:22:12 +0100

    refactor: migrate to material ui

--- src/Components/Repertoire/Movie.js
+++ src/Components/Repertoire/Movie.js
@@ -1,13 +1,17 @@
 import React from "react";
 
-class Movie extends React.Component{
+class Movie extends React.Component
+{
+
+    clickHandler(id){
+
+    }
 
     render() {
         return (
             <>
-                <h1>Movie</h1>
-                <a className = "waves-effect waves-light btn modal-trigger" href = "#modal1" > Modal < /a>
 
+                <a className = "waves-effect waves-light btn modal-trigger" href = "#modal1" > Modal </a>
                     <div id="modal1" className="modal">
                         <div className="modal-content">
                             <h4>Modal Header</h4>
@@ -18,11 +22,10 @@ class Movie extends React.Component{
                         </div>
                     </div>
 
-            </>
+                </>
 
-
-        )
+                    )
     }
-}
+};
 
 export default Movie;
